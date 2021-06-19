08b58d1b5195203c89f24cd1833d2e5d4466c1b8
Han Gao <hga38@sfu.ca>
2021-06-18 19:01:03 -0700

    make stuff work

--- src/main/java/com/example/rectangles/RectangleInfoController.java
+++ src/main/java/com/example/rectangles/RectangleInfoController.java
@@ -1,16 +1,27 @@
 package com.example.rectangles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class RectangleInfoController {
 
-    @RequestMapping("/rectangle")
-    private String getRectanglePage(Model model) {
-        Rectangle rectangle = new Rectangle();
-        model.addAttribute("rectangle", rectangle);
-        return "rectangle";
+    @Autowired
+    RectangleRepository repository;
+
+    @RequestMapping("/rectangle/{rid}")
+    private String getRectanglePage(Model model, @PathVariable long rid) {
+        Optional<Rectangle> result = repository.findById(rid);
+        if (result.isPresent()) {
+            model.addAttribute("rectangle", result.get());
+            return "rectangle";
+        } else {
+            return "error";
+        }
     }
 }
