46ac0588655a8c8ceff1ccf63eb3fde764f36dc0
Han Gao <hga38@sfu.ca>
2021-06-20 18:44:32 -0700

    added jquery and random inputs

--- src/main/resources/static/javascript/form.js
+++ src/main/resources/static/javascript/form.js
@@ -1,4 +1,16 @@
 window.onload = function() {
+
+    let random_name = "rectangle";
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function(data) {
+            const name = data.results[0].name;
+            random_name = name.first + ' ' + name.last;
+        },
+        async: false
+    });
+
     const height = document.getElementById('height');
     const width = document.getElementById('width');
     const color = document.getElementById('color');
@@ -10,6 +22,17 @@ window.onload = function() {
     for (let i = 0; i < inputs.length; i++)
     {
         inputs[i].addEventListener('input', updateDisplay);
+        if (inputs[i].type === 'color') {
+            inputs[i].value = '#' + Math.round(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+        }
+
+        if (inputs[i].type === 'number') {
+            inputs[i].value = Math.round(Math.random() * 1000).toString();
+        }
+
+        if (inputs[i].id === 'name') {
+            inputs[i].value = random_name;
+        }
     }
 
     function updateDisplay(e)
